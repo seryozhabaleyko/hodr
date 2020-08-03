8b15fd6eab535e301840af3b39364d52f820872b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:21:06 +0300

    Create Reviews.js

---
+++ src/pages/Reviews/Reviews.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Reviews.scss';
+
+function Reviews() {
+    return (
+        <div className="container">
+            <div className="reviews-page__heading">
+                <h1 className="reviews-page__title">Рецензии</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Reviews;
