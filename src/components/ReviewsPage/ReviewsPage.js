2e100869c550882f7de0dc4b84b03527f1bd2fd0
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:10:46 +0300

    Create ReviewsPage.js

---
+++ src/components/ReviewsPage/ReviewsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ReviewsPage.scss';
+
+function ReviewsPage() {
+    return (
+        <div className="container">
+            <div className="reviews-page__heading">
+                <h1 className="reviews-page__title">Рецензии</h1>
+            </div>
+        </div>
+    );
+}
+
+export default ReviewsPage;
