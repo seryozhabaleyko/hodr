c2a824638451621febe93932b9ef598fb50233c6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:10:52 +0300

    Create UserAccount.js

---
+++ src/components/UserAccount/UserAccount.js
@@ -0,0 +1,24 @@
+import React, { memo } from 'react';
+import { Avatar, Badge } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import { auth } from '../../services/firebase';
+
+import './UserAccount.scss';
+
+function UserAccount() {
+    return auth().currentUser ? (
+        <Badge count={0}>
+            <Avatar
+                src={auth().currentUser.photoURL}
+                size={32}
+                icon={<UserOutlined />}
+                alt={auth().currentUser.displayName}
+            />
+        </Badge>
+    ) : (
+        <Avatar size={32} icon={<UserOutlined />} />
+    );
+}
+
+export default memo(UserAccount);
