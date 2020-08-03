e172cb9926e975bcc333754e6c57aefcf55722cd
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:09:53 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './footer.scss';
+import { memo } from 'react';
 
 function Footer() {
     return <footer className="footer">footer</footer>;
 }
 
-export default Footer;
+export default memo(Footer);
