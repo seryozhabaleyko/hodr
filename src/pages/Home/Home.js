60de7cfc7258d2a5aaebc4c3117fd8902a380015
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:11:04 +0300

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Home.scss';
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
