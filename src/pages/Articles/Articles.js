4daefeba8337d26790d423942d5d1dd726db6af8
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:19:46 +0300

    Create Articles.js

---
+++ src/pages/Articles/Articles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Articles.scss';
+
+function Articles() {
+    return (
+        <div className="container">
+            <div className="articles-page__heading">
+                <h1>Статьи</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Articles;
