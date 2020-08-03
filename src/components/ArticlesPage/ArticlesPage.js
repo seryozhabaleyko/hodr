a1f0d8b9a5edaca1eb5879d03755b714e3d3c43a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:09:35 +0300

    Create ArticlesPage.js

---
+++ src/components/ArticlesPage/ArticlesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ArticlesPage.scss';
+
+function ArticlesPage() {
+    return (
+        <div className="container">
+            <div className="articles-page__heading">
+                <h1>Статьи</h1>
+            </div>
+        </div>
+    );
+}
+
+export default ArticlesPage;
