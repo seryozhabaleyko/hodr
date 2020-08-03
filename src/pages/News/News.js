c2c735749a608eda0dac6dcbffc0fe24d77dcd0d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-03 18:18:00 +0300

    Create News.js

---
+++ src/pages/News/News.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './News.scss';
+
+function News() {
+    return (
+        <div className="container">
+            <div className="news-page__heading">
+                <h1 className="news-page__title">Новости</h1>
+            </div>
+        </div>
+    );
+}
+
+export default News;
