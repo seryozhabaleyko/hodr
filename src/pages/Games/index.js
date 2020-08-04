e3e8353db037f70770edd3520f78f66e2589d17c
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-04 17:53:56 +0300

    Update index.js

--- src/pages/Games/index.js
+++ src/pages/Games/index.js
@@ -1,3 +1,8 @@
 import Games from './Games';
+import * as actions from './actions';
+import * as constants from './constants';
+import reducer from './reducer';
+import * as selectors from './selectors';
 
+export { actions, constants, reducer, selectors };
 export default Games;
