af2bcc00cf3540aedb4d1e4ad7e4ab55f3da585b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-06 15:22:30 +0300

    Update UserAccount.js

--- src/components/UserAccount/UserAccount.js
+++ src/components/UserAccount/UserAccount.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Avatar, Badge } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
 
 import { auth } from '../../services/firebase';
 
 import './UserAccount.scss';
 
-function UserAccount() {
-    return auth().currentUser ? (
-        <Badge count={0}>
+/* <Badge count={0}>
             <Avatar
                 src={auth().currentUser.photoURL}
                 size={32}
                 icon={<UserOutlined />}
                 alt={auth().currentUser.displayName}
             />
-        </Badge>
-    ) : (
-        <Avatar size={32} icon={<UserOutlined />} />
-    );
+        </Badge> */
+
+function UserAccount() {
+    return auth().currentUser ? 1 : 2;
 }
 
 export default UserAccount;
