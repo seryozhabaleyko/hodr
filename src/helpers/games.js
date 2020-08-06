1eae7e14f205f5a9d5f1ce2f5b48e95c4162ef63
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-06 15:22:32 +0300

    Update games.js

--- src/helpers/games.js
+++ src/helpers/games.js
@@ -1,22 +1,26 @@
-import { db, storage } from '../services/firebase';
-
-storage
-    .collection('users')
-    .add({
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815,
-    })
-    .then(function (docRef) {
-        console.log('Document written with ID: ', docRef.id);
-    })
-    .catch(function (error) {
-        console.error('Error adding document: ', error);
-    });
+import { db } from '../services/firebase';
 
 export function fetchGamesApi() {
     return db
         .ref('games')
+        .once('value')
+        .then((snapshot) => {
+            return snapshot.val();
+        });
+}
+
+export function fetchGenresApi() {
+    return db
+        .ref('genres/games')
+        .once('value')
+        .then((snapshot) => {
+            return snapshot.val();
+        });
+}
+
+export function postGenreApi() {
+    return db
+        .ref('genres/games')
         .once('value')
         .then((snapshot) => {
             return snapshot.val();
