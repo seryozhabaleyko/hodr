d1b3539944de78309debb400a6d9f7fb3e9b5dc2
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-07 02:20:45 +0300

    Update Games.js

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+import Slick from 'react-slick';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import GameCard from '../../components/GameCard';
@@ -19,6 +22,14 @@ function Games({ fetchGames, games }) {
 
     console.log('data', data);
 
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+    };
+
     return (
         <div className="container">
             <section className="games-page__new gp-new">
@@ -28,11 +39,17 @@ function Games({ fetchGames, games }) {
                         <span>Посмотреть все</span>
                     </Link>
                 </header>
-                {/* <div className="gp-new__list gp-new__grid">
-                    {[...Array(6)].map((dqd, index) => (
-                        <GameCard key={index} />
-                    ))}
-                </div> */}
+
+                <Slick {...settings}>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                    <div>7</div>
+                    <div>8</div>
+                </Slick>
 
                 <Swiper spaceBetween={30} slidesPerView={7} pagination={{ clickable: true }}>
                     {[...data, ...Array(10)].map((game, index) => (
