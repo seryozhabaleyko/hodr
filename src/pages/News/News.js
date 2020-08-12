11de0ae6816a6599adcb6c0df9a57821b778f412
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-12 16:05:03 +0300

    Update News.js

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+
+import { fetchNews } from './actions';
+import { getNews } from './selectors';
+import NewsCard, { NewsCardSkeleton } from './components/NewsCard';
+import Select from '../../components/Select';
 
 import './News.scss';
 
+function NewsList() {
+    const { loading, data, error } = useSelector(getNews, shallowEqual);
+
+    if (loading) {
+        return (
+            <div className="news-page__list news-page__grid">
+                {[...Array(20)].map((el, index) => (
+                    <NewsCardSkeleton key={index} />
+                ))}
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="error__message">{error.message}</p>;
+    }
+
+    if (data.length === 0) {
+        return (
+            <p className="warning__message">
+                Упс! У нас нет таких товаров, попробуйте изменить условия поиска.
+            </p>
+        );
+    }
+
+    return (
+        <div className="news-page__list news-page__grid">
+            {data.map((news) => (
+                <NewsCard {...news} key={news.id} />
+            ))}
+        </div>
+    );
+}
+
+function Filters() {
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' },
+    ];
+
+    return <Select options={options} />;
+}
+
 function News() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchNews());
+    }, [dispatch]);
+
     return (
         <div className="container">
-            <div className="news-page__heading">
+            <header className="news-page__heading">
                 <h1 className="news-page__title">Новости</h1>
-            </div>
+                <Filters />
+            </header>
+
+            <NewsList />
         </div>
     );
 }
