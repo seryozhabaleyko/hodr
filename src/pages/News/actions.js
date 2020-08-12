52364ab0e0c4bc46a32059051f8cc2484c27c654
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-13 01:52:38 +0300

    Update actions.js

--- src/pages/News/actions.js
+++ src/pages/News/actions.js
@@ -1,5 +1,10 @@
-import { FETCH_NEWS_SUCCESS, FETCH_NEWS_FAILURE } from './actionTypes';
-import { fetchNewsApi } from '../../helpers/news';
+import {
+    FETCH_NEWS_SUCCESS,
+    FETCH_NEWS_FAILURE,
+    CREATE_NEWS_SUCCESS,
+    CREATE_NEWS_FAILURE,
+} from './actionTypes';
+import { fetchNewsApi, cerateNewsApi } from '../../helpers/news';
 
 const fetchNewsSuccess = (data) => ({
     type: FETCH_NEWS_SUCCESS,
@@ -18,5 +23,24 @@ export const fetchNews = () => async (dispatch) => {
         dispatch(fetchNewsSuccess(response));
     } catch (error) {
         dispatch(fetchNewsFailure(error));
+    }
+};
+
+const cerateNewsSuccess = (data) => ({
+    type: CREATE_NEWS_SUCCESS,
+    payload: data,
+});
+
+const cerateNewsFailure = (error) => ({
+    type: CREATE_NEWS_FAILURE,
+    payload: error,
+});
+
+export const cerateNews = (news) => async (dispatch) => {
+    try {
+        const response = await cerateNewsApi(news);
+        dispatch(cerateNewsSuccess(response));
+    } catch (error) {
+        dispatch(cerateNewsFailure());
     }
 };
