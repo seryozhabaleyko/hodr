fce96fd37b87a7512086f0a630464eb94f850b53
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-13 01:52:40 +0300

    Update news.js

--- src/helpers/news.js
+++ src/helpers/news.js
@@ -6,3 +6,30 @@ export function fetchNewsApi() {
         .get()
         .then((querySnapshot) => querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 }
+
+export function cerateNewsApi(data) {
+    return firestore
+        .collection('news')
+        .add(data)
+        .then((docRef) => docRef.get())
+        .then(mapDoc);
+}
+
+export function fetchNewsSingleApi(slug) {
+    return firestore.collection('news').where('slug', '==', slug).get().then(mapSnapshot);
+}
+
+function mapSnapshot(snapshot) {
+    return snapshot.docs.map(mapDoc);
+}
+
+function mapRef(ref) {
+    return ref.get().then(mapDoc);
+}
+
+function mapDoc(doc) {
+    return {
+        id: doc.id,
+        ...doc.data(),
+    };
+}
