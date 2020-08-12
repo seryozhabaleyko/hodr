d9181bfee549892072625c2034c54ad1b8e44e13
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-12 16:03:32 +0300

    Create AdminGames.js

---
+++ src/pages/Admin/pages/AdminGames/AdminGames.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminGames() {
+    return (
+        <div className="container">
+            <h1>AdminGames</h1>
+        </div>
+    );
+}
+
+export default AdminGames;
