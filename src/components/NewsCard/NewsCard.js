c995a89db9ed2d4eb5cc251c737f33a04e4191d2
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-13 01:52:46 +0300

    Update NewsCard.js

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './NewsCard.scss';
 
-function NewsCard() {
-    return <div className="news-card">news card</div>;
+function NewsCard({ title, slug, createdAt }) {
+    return (
+        <article className="news-card">
+            <div className="news-card__image"></div>
+            <div className="news-card__content">
+                <Link to={`news/${slug}`}>
+                    <h3 className="news-card__title">{title}</h3>
+                </Link>
+                <time className="news-card__date-time" dateTime={createdAt}>
+                    {createdAt}
+                </time>
+            </div>
+        </article>
+    );
 }
 
 export default NewsCard;
