04d44bee9bed0e9c974702e8b1e86a393a027177
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-12 16:02:58 +0300

    Create ScreenshotCard.js

---
+++ src/components/ScreenshotCard/ScreenshotCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ScreenshotCard.scss';
+
+function ScreenshotCard() {
+    return <div className="screenshot-card">screenshot card</div>;
+}
+
+export default ScreenshotCard;
