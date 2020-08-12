3e4d479ecfebf421e4bba4f781d71aa207bdb7f0
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-12 16:05:48 +0300

    Create ArticleCard.js

---
+++ src/components/ArticleCard/ArticleCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ArticleCard.scss';
+
+function ArticleCard() {
+    return <div className="article-card">article card</div>;
+}
+
+export default ArticleCard;
