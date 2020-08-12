a6f01fd699a922faa6ac804890bab83c55252d29
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-13 01:52:28 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux';
 import games from '../pages/Games/reducer';
 import game from '../pages/Game/reducer';
 import news from '../pages/News/reducer';
+import newsSingle from '../pages/NewsSingle/reducer';
 import articles from '../pages/Articles/reducer';
 import reviews from '../pages/Reviews/reducer';
 
-export default combineReducers({ games, game, news, articles, reviews });
+export default combineReducers({ games, game, news, newsSingle, articles, reviews });
