bbc9d59302389f01f64b11104216441a6602d2a4
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-12 16:03:38 +0300

    Create AdminNews.js

---
+++ src/pages/Admin/pages/AdminNews/AdminNews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminNews() {
+    return (
+        <div className="container">
+            <h1>AdminNews</h1>
+        </div>
+    );
+}
+
+export default AdminNews;
