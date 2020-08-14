fd3813e49957e71ec4d616b58fbc17a11f2e1e4f
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-14 15:16:14 +0300

    Create reducer.js

---
+++ src/pages/UserEdit/reducer.js
@@ -0,0 +1,20 @@
+import { UPDATE_USER_REQUEST, UPDATE_USER_SUCCESS, UPDATE_USER_FAILURE } from './actionTypes';
+
+const initialState = {
+    loading: true,
+    error: null,
+};
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        case UPDATE_USER_REQUEST:
+            return { ...state, loading: true, error: null };
+        case UPDATE_USER_SUCCESS:
+            return { ...state, loading: false };
+        case UPDATE_USER_FAILURE:
+            return { ...state, loading: false, error: payload };
+
+        default:
+            return state;
+    }
+};
