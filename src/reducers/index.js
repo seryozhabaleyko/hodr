b1b933e29bb25bd96549311123f7cdd94adcda45
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-15 01:27:25 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,5 +7,15 @@ import newsSingle from '../pages/NewsSingle/reducer';
 import articles from '../pages/Articles/reducer';
 import reviews from '../pages/Reviews/reducer';
 import user from '../pages/User/reducer';
+import editUser from '../pages/UserEdit/reducer';
 
-export default combineReducers({ games, game, news, newsSingle, articles, reviews, user });
+export default combineReducers({
+    games,
+    game,
+    news,
+    newsSingle,
+    articles,
+    reviews,
+    user,
+    editUser,
+});
