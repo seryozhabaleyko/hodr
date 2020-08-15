cf662c2afab77899264d430ec3a6faef95dfd73b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-16 00:12:15 +0300

    Update reducer.js

--- src/pages/News/reducer.js
+++ src/pages/News/reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_NEWS_SUCCESS, FETCH_NEWS_FAILURE } from './actionTypes';
+import { FETCH_NEWS_REQUEST, FETCH_NEWS_SUCCESS, FETCH_NEWS_FAILURE } from './actionTypes';
 
 const initialState = {
     loading: true,
@@ -8,6 +8,8 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
+        case FETCH_NEWS_REQUEST:
+            return { ...state, loading: true, error: null };
         case FETCH_NEWS_SUCCESS:
             return { ...state, loading: false, data: payload };
         case FETCH_NEWS_FAILURE:
