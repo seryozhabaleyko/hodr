740aa2c11aa3e6d496895bdc2b2915c888df5394
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-15 16:05:12 +0300

    Create index.js

---
+++ src/components/ui/index.js
@@ -0,0 +1,6 @@
+export { Input } from './Input';
+export { InputField } from './InputField';
+export { Avatar } from './Avatar';
+export { Spinner } from './Spinner';
+export { Select } from './Select';
+export { Button } from './Button';
