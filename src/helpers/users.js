4e5de0138b358d287b22265dbb9cb982f041b68a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-15 16:05:48 +0300

    Update users.js

--- src/helpers/users.js
+++ src/helpers/users.js
@@ -1,13 +1,22 @@
-import { firestore } from '../services/firebase';
+import { auth, firestore } from '../services/firebase';
 
-export function fetchUserApi(userId) {
-    return firestore
+export async function fetchUserApi(username) {
+    const user = await firestore
         .collection('users')
-        .doc(userId)
+        .where('username', '==', username)
         .get()
-        .then((doc) => ({ ...doc.data(), id: doc.id }));
+        .then((snapshot) =>
+            snapshot.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            })),
+        );
+
+    return user[0];
 }
 
 export function updateUserApi(userId, data) {
+    const displayName = `${data.name} ${data.surname}`;
+    auth().currentUser.updateProfile({ displayName, photoURL: data.avatar });
     return firestore.collection('users').doc(userId).update(data);
 }
