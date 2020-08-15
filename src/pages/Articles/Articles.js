38a449e85cf9595a551da8a4b7a3cdcaadfabefd
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-16 02:25:12 +0300

    Update Articles.js

--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import Select from 'react-select';
+import { Select } from 'antd';
 
 import { fetchArticles } from './actions';
 import { getArticles } from './selectors';
@@ -14,17 +14,11 @@ function Articles() {
         dispatch(fetchArticles());
     }, [dispatch]);
 
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
-    ];
-
     return (
         <div className="container">
             <header className="articles-page__heading">
                 <h1 className="articles-page__title">Статьи</h1>
-                <Select className="articles-page__filters" options={options} />
+                <Filters />
             </header>
             <ArticlesList />
         </div>
@@ -65,6 +59,36 @@ function ArticleCard({ title, image }) {
                 <h1 className="ap_card__title">{title}</h1>
             </div>
         </article>
+    );
+}
+
+function Filters() {
+    const options = [
+        { value: 'all', label: 'Все' },
+        { value: 'games', label: 'Игры' },
+        { value: 'movieAndSeries', label: 'Кино и сериалы' },
+        { value: 'comicsAndBooks', label: 'Комиксы и книги' },
+        { value: 'internet', label: 'Интернет' },
+        { value: 'technology', label: 'Технологии' },
+        { value: 'cybersport', label: 'Киберспорт' },
+        { value: 'life', label: 'Жизнь' },
+        { value: 'music', label: 'Музыка' },
+        { value: 'specialProjects', label: 'Спецпроекты' },
+        { value: 'auto', label: 'Авто' },
+    ];
+
+    const handleChange = (value) => {
+        console.log(`selected ${value}`);
+    };
+
+    return (
+        <Select defaultValue="all" style={{ minWidth: '160px' }} onChange={handleChange}>
+            {options.map(({ value, label }, i) => (
+                <Select.Option value={value} key={i}>
+                    {label}
+                </Select.Option>
+            ))}
+        </Select>
     );
 }
 
