4b4415434920292fb03d6e3a4a2350f4c08a0388
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-15 16:05:44 +0300

    Create formatDate.js

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,61 @@
+export function formatDate(date) {
+    let diff = new Date() - date; // разница в миллисекундах
+
+    if (diff < 1000) {
+        // меньше 1 секунды
+        return 'прямо сейчас';
+    }
+
+    let sec = Math.floor(diff / 1000); // преобразовать разницу в секунды
+
+    if (sec < 60) {
+        return sec + ' сек. назад';
+    }
+
+    let min = Math.floor(diff / 60000); // преобразовать разницу в минуты
+    if (min < 60) {
+        return min + ' мин. назад';
+    }
+
+    // отформатировать дату
+    // добавить ведущие нули к единственной цифре дню/месяцу/часам/минутам
+    let d = date;
+    d = [
+        '0' + d.getDate(),
+        '0' + (d.getMonth() + 1),
+        '' + d.getFullYear(),
+        '0' + d.getHours(),
+        '0' + d.getMinutes(),
+    ].map((component) => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
+
+    // соединить компоненты в дату
+    return d.slice(0, 3).join('.') + ' в ' + d.slice(3).join(':');
+}
+
+function formatDate3(date) {
+    let dayOfMonth = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let diffMs = new Date() - date;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = diffSec / 60;
+    let diffHour = diffMin / 60;
+
+    year = year.toString().slice(-2);
+    month = month < 10 ? '0' + month : month;
+    dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    if (diffSec < 1) {
+        return 'прямо сейчас';
+    } else if (diffMin < 1) {
+        return `${diffSec} сек. назад`;
+    } else if (diffHour < 1) {
+        return `${diffMin} мин. назад`;
+    } else {
+        return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`;
+    }
+}
