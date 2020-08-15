fa4cc656c87005de2943a9f7470df761ec824970
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-16 00:12:30 +0300

    Update Select.js

--- src/components/ui/Select/Select.js
+++ src/components/ui/Select/Select.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './Select.scss';
 
-function Select({ options = [] }) {
+function Select({
+    className,
+    defaultValue,
+    disabled,
+    onChange,
+    value,
+    height = 32,
+    options = [],
+    ...restProps
+}) {
+    const classes = classNames('select', className);
+
     return (
-        <select className="select">
-            {options.map(({ value, label }) => (
-                <option value={value} key={value}>
-                    {label}
-                </option>
-            ))}
-        </select>
+        <div className="select-wrapper" style={{ height }} {...restProps}>
+            <select
+                className={classes}
+                style={{ paddingLeft: '10px', paddingRight: '28px' }}
+                disabled={disabled}
+                defaultValue={defaultValue}
+                value={value}
+                onChange={onChange}
+            >
+                {options.map(({ value, label }) => (
+                    <option value={value} key={value}>
+                        {label}
+                    </option>
+                ))}
+            </select>
+        </div>
     );
 }
+
+Select.propTypes = {
+    defaultValue: PropTypes.any,
+    value: PropTypes.any,
+    onChange: PropTypes.func,
+    required: PropTypes.bool,
+};
 
 export default Select;
