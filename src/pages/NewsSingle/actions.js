d5603e109550564c6639a9a246e4caa4c621212c
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-16 00:12:09 +0300

    Update actions.js

--- src/pages/NewsSingle/actions.js
+++ src/pages/NewsSingle/actions.js
@@ -1,5 +1,13 @@
-import { FETCH_NEWS_SINGLE_SUCCESS, FETCH_NEWS_SINGLE_FAILURE } from './actionTypes';
+import {
+    FETCH_NEWS_SINGLE_REQUEST,
+    FETCH_NEWS_SINGLE_SUCCESS,
+    FETCH_NEWS_SINGLE_FAILURE,
+} from './actionTypes';
 import { fetchNewsSingleApi } from '../../helpers/news';
+
+const fetchNewsSingleRequest = () => ({
+    type: FETCH_NEWS_SINGLE_REQUEST,
+});
 
 const fetchNewsSingleSuccess = (data) => ({
     type: FETCH_NEWS_SINGLE_SUCCESS,
@@ -12,6 +20,7 @@ const fetchNewsSingleFailure = (error) => ({
 });
 
 export const fetchNewsSingle = (slug) => async (dispatch) => {
+    dispatch(fetchNewsSingleRequest());
     try {
         const response = await fetchNewsSingleApi(slug);
         dispatch(fetchNewsSingleSuccess(response));
