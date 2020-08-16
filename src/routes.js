8a96ea8d9b451f57ef6aaebe5dbe7e8409ec3769
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-17 02:02:38 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,8 @@
 import Admin from './pages/Admin';
 import AdminNews from './pages/Admin/pages/AdminNews';
 import AdminGames from './pages/Admin/pages/AdminGames';
-/* import Game from './pages/Game';
-import GameVideos from './pages/Game/GameVideos'; */
-
-import NewsSingle from './pages/NewsSingle';
 
 const routes = [
-    /* {
-        path: '/game/:slug',
-        component: Game,
-        routes: [
-            {
-                path: '/game/:slug/videos',
-                component: GameVideos,
-            },
-        ],
-    }, */
-    {
-        path: '/news/:slug',
-        component: NewsSingle,
-    },
     {
         path: '/admin',
         component: Admin,
