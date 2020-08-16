d6871dde3c39aaac22175e46b9f2086defade816
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-16 20:21:08 +0300

    Update reducer.js

--- src/pages/Games/reducer.js
+++ src/pages/Games/reducer.js
@@ -1,4 +1,5 @@
 import {
+    FETCH_GAMES_REQUEST,
     FETCH_GAMES_SUCCESS,
     FETCH_GAMES_FAILURE,
     FETCH_GENRES_SUCCESS,
@@ -18,6 +19,8 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
+        case FETCH_GAMES_REQUEST:
+            return { ...state, loading: true, error: null };
         case FETCH_GAMES_SUCCESS:
             return { ...state, loading: false, data: payload };
         case FETCH_GAMES_FAILURE:
