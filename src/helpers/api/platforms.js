3816cf7850b934cd4e51eeba85691bd7219c2777
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-19 02:10:49 +0300

    Update platforms.js

--- src/helpers/api/platforms.js
+++ src/helpers/api/platforms.js
@@ -3,6 +3,7 @@ import { firestore } from '../../services/firebase';
 export function fetchPlatformsApi() {
     return firestore
         .collection('platforms')
+        .orderBy('label')
         .get()
         .then((querySnapshot) => querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 }
