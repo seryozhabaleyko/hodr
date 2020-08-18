fbd64bdeec03b39421ba2be85384846dc14ae686
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-18 12:58:13 +0300

    Update constants.js

---
+++ src/pages/News/constants.js
@@ -0,0 +1,13 @@
+export const options = [
+    { value: 'all', label: 'Все' },
+    { value: 'games', label: 'Игры' },
+    { value: 'movie-and-series', label: 'Кино и сериалы' },
+    { value: 'comics-and-books', label: 'Комиксы и книги' },
+    { value: 'internet', label: 'Интернет' },
+    { value: 'technology', label: 'Технологии' },
+    { value: 'cybersport', label: 'Киберспорт' },
+    { value: 'life', label: 'Жизнь' },
+    { value: 'music', label: 'Музыка' },
+    { value: 'special-projects', label: 'Спецпроекты' },
+    { value: 'auto', label: 'Авто' },
+];
