a6f5b327a27061507235570191c6375c4e38c591
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-19 02:10:51 +0300

    Update genres.js

--- src/helpers/api/genres.js
+++ src/helpers/api/genres.js
@@ -3,6 +3,7 @@ import { firestore } from '../../services/firebase';
 export function fetchGenresApi() {
     return firestore
         .collection('genres')
+        .orderBy('label')
         .get()
         .then((querySnapshot) => querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 }
