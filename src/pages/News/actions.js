909bb8182c1923b00908370ea4b40d9398050d87
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-18 12:58:09 +0300

    Update actions.js

--- src/pages/News/actions.js
+++ src/pages/News/actions.js
@@ -2,10 +2,14 @@ import {
     FETCH_NEWS_REQUEST,
     FETCH_NEWS_SUCCESS,
     FETCH_NEWS_FAILURE,
+    FETCH_NEWS_BY_CATEGORY_REQUEST,
+    FETCH_NEWS_BY_CATEGORY_SUCCESS,
+    FETCH_NEWS_BY_CATEGORY_FAILURE,
     CREATE_NEWS_SUCCESS,
     CREATE_NEWS_FAILURE,
+    SET_CURRENT_PAGE,
 } from './actionTypes';
-import { fetchNewsApi, cerateNewsApi } from '../../helpers/news';
+import { fetchNewsApi, cerateNewsApi, fetchNewsByCategoryApi } from '../../helpers/news';
 
 const fetchNewsRequest = () => ({
     type: FETCH_NEWS_REQUEST,
@@ -32,6 +36,31 @@ export const fetchNews = () => async (dispatch) => {
     }
 };
 
+const fetchNewsByCategoryRequest = () => ({
+    type: FETCH_NEWS_BY_CATEGORY_REQUEST,
+});
+
+const fetchNewsByCategorySuccess = (data) => ({
+    type: FETCH_NEWS_BY_CATEGORY_SUCCESS,
+    payload: data,
+});
+
+const fetchNewsByCategoryFailure = (error) => ({
+    type: FETCH_NEWS_BY_CATEGORY_FAILURE,
+    payload: error,
+});
+
+export const fetchNewsByCategory = (category) => async (dispatch) => {
+    dispatch(fetchNewsByCategoryRequest());
+    try {
+        const response = await fetchNewsByCategoryApi(category);
+
+        dispatch(fetchNewsByCategorySuccess(response));
+    } catch (error) {
+        dispatch(fetchNewsByCategoryFailure(error));
+    }
+};
+
 const cerateNewsSuccess = (data) => ({
     type: CREATE_NEWS_SUCCESS,
     payload: data,
@@ -50,3 +79,8 @@ export const cerateNews = (news) => async (dispatch) => {
         dispatch(cerateNewsFailure());
     }
 };
+
+export const setCurrentPage = (currentPage) => ({
+    type: SET_CURRENT_PAGE,
+    payload: currentPage,
+});
