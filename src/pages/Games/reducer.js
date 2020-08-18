e497eab6aeea991905b0b18b5c67ea99fb50b792
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-18 18:24:36 +0300

    Update reducer.js

--- src/pages/Games/reducer.js
+++ src/pages/Games/reducer.js
@@ -4,15 +4,24 @@ import {
     FETCH_GAMES_FAILURE,
     FETCH_GENRES_SUCCESS,
     FETCH_GENRES_FAILURE,
+    FETCH_GENRES_REQUEST,
+    FETCH_PLATFORMS_REQUEST,
+    FETCH_PLATFORMS_SUCCESS,
+    FETCH_PLATFORMS_FAILURE,
 } from './actionTypes';
 
 const initialState = {
     loading: true,
     data: [],
     error: null,
+    platforms: {
+        loading: true,
+        data: [],
+        error: null,
+    },
     genres: {
         loading: true,
-        data: {},
+        data: [],
         error: null,
     },
 };
@@ -26,10 +35,19 @@ export default (state = initialState, { type, payload }) => {
         case FETCH_GAMES_FAILURE:
             return { ...state, loading: false, error: payload };
 
+        case FETCH_PLATFORMS_REQUEST:
+            return { ...state, platforms: { ...state.platforms, loading: true, error: null } };
+        case FETCH_PLATFORMS_SUCCESS:
+            return { ...state, platforms: { ...state.platforms, loading: false, data: payload } };
+        case FETCH_PLATFORMS_FAILURE:
+            return { ...state, platforms: { ...state.platforms, loading: false, error: payload } };
+
+        case FETCH_GENRES_REQUEST:
+            return { ...state, genres: { ...state.genres, loading: true, error: null } };
         case FETCH_GENRES_SUCCESS:
-            return { ...state, genres: { loading: false, data: payload } };
+            return { ...state, genres: { ...state.genres, loading: false, data: payload } };
         case FETCH_GENRES_FAILURE:
-            return { ...state, genres: { loading: false, error: payload } };
+            return { ...state, genres: { ...state.genres, loading: false, error: payload } };
 
         default:
             return state;
