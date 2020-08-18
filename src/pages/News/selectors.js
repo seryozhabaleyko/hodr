4843b96846e2ddd8f51ac877b9136748bd5d570d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-18 12:58:21 +0300

    Update selectors.js

--- src/pages/News/selectors.js
+++ src/pages/News/selectors.js
@@ -4,13 +4,9 @@ const newsLoadingSelector = (state) => state.news.loading;
 const newsItemsSelector = (state) => state.news.data;
 const newsErrorSelector = (state) => state.news.error;
 
-const getNewsItemsSelector = createSelector(newsItemsSelector, (items) =>
-    items.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)),
-);
-
 export const getNews = createSelector(
     newsLoadingSelector,
-    getNewsItemsSelector,
+    newsItemsSelector,
     newsErrorSelector,
     (loading, data, error) => ({ loading, data, error }),
 );
