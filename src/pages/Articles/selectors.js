6f58d2acfe47f11a0a6e0a09101d9a865be69085
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-18 12:58:06 +0300

    Update selectors.js

--- src/pages/Articles/selectors.js
+++ src/pages/Articles/selectors.js
@@ -1,1 +1,16 @@
-export const getArticles = (state) => state.articles;
+import { createSelector } from 'reselect';
+
+const articlesLoading = (state) => state.articles.loading;
+const articlesItems = (state) => state.articles.data;
+const articlesError = (state) => state.articles.error;
+
+const sortByDateArticlesItems = createSelector(articlesItems, (items) =>
+    items.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)),
+);
+
+export const getArticles = createSelector(
+    articlesLoading,
+    sortByDateArticlesItems,
+    articlesError,
+    (loading, items, error) => ({ loading, items, error }),
+);
