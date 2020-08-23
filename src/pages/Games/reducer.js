0f76f0646e3442108090c85ec53274ed84dfaea6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-23 23:01:29 +0300

    Update reducer.js

--- src/pages/Games/reducer.js
+++ src/pages/Games/reducer.js
@@ -8,18 +8,16 @@ import {
     FETCH_PLATFORMS_REQUEST,
     FETCH_PLATFORMS_SUCCESS,
     FETCH_PLATFORMS_FAILURE,
-    SET_RATINGS_VISIBILITY_FILTER,
-    SET_YEARS_VISIBILITY_FILTER,
+    SET_FILTER_BY_RATING,
+    SET_FILTER_BY_YEAR,
 } from './actionTypes';
 
 const initialState = {
     loading: true,
     data: [],
     error: null,
-    visibilityFilters: {
-        ratings: 'all',
-        years: 'all',
-    },
+    filterByRating: 'all',
+    filterByYear: 'all',
     platforms: {
         loading: true,
         data: [],
@@ -55,16 +53,10 @@ export default (state = initialState, { type, payload }) => {
         case FETCH_GENRES_FAILURE:
             return { ...state, genres: { ...state.genres, loading: false, error: payload } };
 
-        case SET_RATINGS_VISIBILITY_FILTER:
-            return {
-                ...state,
-                visibilityFilters: { ...state.visibilityFilters, ratings: payload },
-            };
-        case SET_YEARS_VISIBILITY_FILTER:
-            return {
-                ...state,
-                visibilityFilters: { ...state.visibilityFilters, years: payload },
-            };
+        case SET_FILTER_BY_RATING:
+            return { ...state, filterByRating: payload };
+        case SET_FILTER_BY_YEAR:
+            return { ...state, filterByYear: payload };
 
         default:
             return state;
