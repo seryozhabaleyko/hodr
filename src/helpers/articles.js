586c41b1f20dc5f502e2a3720c6c69ec31af72d5
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-25 01:04:57 +0300

    Delete articles.js

--- src/helpers/articles.js
+++
@@ -1,8 +0,0 @@
-import { firestore } from '../services/firebase';
-
-export function fetchArticlesApi() {
-    return firestore
-        .collection('articles')
-        .get()
-        .then((querySnapshot) => querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-}
