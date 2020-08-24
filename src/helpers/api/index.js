6e2d3062c209ffe1e08ec1a49033364bad20f342
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-25 01:04:55 +0300

    Update index.js

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -1,2 +1,4 @@
 export { fetchPlatformsApi } from './platforms';
 export { fetchGenresApi } from './genres';
+export { fetchArticlesApi } from './articles';
+export { fetchGamesApi, fetchGameApi } from './games';
