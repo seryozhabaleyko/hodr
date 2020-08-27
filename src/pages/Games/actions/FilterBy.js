ba01b00a85a47218b883abd4db69a6256b8a0a22
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-27 20:45:56 +0300

    Update FilterBy.js

--- src/pages/Games/actions/FilterBy.js
+++ src/pages/Games/actions/FilterBy.js
@@ -1,5 +1,9 @@
-import { SET_FILTER_BY_RATING, SET_FILTER_BY_YEAR } from '../actionTypes';
+import { SET_FILTER_BY_GENRE, SET_FILTER_BY_RATING, SET_FILTER_BY_YEAR } from '../actionTypes';
 
+export const setFilterByGenre = (filter) => ({
+    type: SET_FILTER_BY_GENRE,
+    payload: filter,
+});
 export const setFilterByRating = (filter) => ({
     type: SET_FILTER_BY_RATING,
     payload: filter,
