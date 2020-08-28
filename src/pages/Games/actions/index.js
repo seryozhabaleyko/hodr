b88c0665fd19012aea9df524ac73bb5dda1ce4a1
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-28 17:47:28 +0300

    Update index.js

--- src/pages/Games/actions/index.js
+++ src/pages/Games/actions/index.js
@@ -1,4 +1,5 @@
 export { fetchPlatforms } from './platforms';
 export { fetchGenres } from './genres';
 export { fetchGames } from './games';
+export { fetchCollectionNewGames, fetchCollectionPopularGames } from './collections';
 export { setFilterByRating, setFilterByYear, setFilterByGenre } from './FilterBy';
