13d28bb8f52dabc2808b6cb171f0020518cebe48
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-08-31 21:47:54 +0300

    Update NewsCard.js

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
-import 'moment/locale/ru';
+import { Badge } from 'antd';
 
-import { formatDate } from '../../helpers/formatDate';
+import moment from '../../helpers/moment';
 
 import './NewsCard.scss';
 
-moment.locale('ru');
-
 function NewsCard({
-    title = 'something',
+    title = 'Каждый день миллионы людей ищут фильмы онлайн, и никто не хочет усложнять себе жизнь – и вы наверняка один из них!',
     slug,
     photoUrl = 'https://thumbs.dfs.ivi.ru/storage2/contents/d/0/864e981de08ff8e31e5e352073121c.jpg/285x136/',
     createdAt,
+    category = { label: 'Значение', value: 'value', color: 'cyan' },
 }) {
     return (
         <article className="news-card">
-            <figure className="news-card__image">
-                <img src={photoUrl} alt={title} />
-            </figure>
-            <div className="news-card__content">
-                <Link to={`news/${slug}`}>
+            <Link to={`/news/${slug}`}>
+                <figure className="news-card__image">
+                    <Badge.Ribbon
+                        className="news-card__category"
+                        color={category.color}
+                        placement="start"
+                        text={category.label}
+                    />
+                    <picture>
+                        <img src={photoUrl} alt={title} />
+                    </picture>
+                </figure>
+                <div className="news-card__content">
                     <h3 className="news-card__title">{title}</h3>
-                </Link>
-                <time className="news-card__date-time" dateTime={createdAt}>
-                    {formatDate(new Date(createdAt))}
-                    <br />
-                    {moment().calendar()}
-                </time>
-            </div>
+
+                    <time className="news-card__date-time" dateTime={createdAt}>
+                        {moment(createdAt).calendar()}
+                    </time>
+                </div>
+            </Link>
         </article>
     );
 }
